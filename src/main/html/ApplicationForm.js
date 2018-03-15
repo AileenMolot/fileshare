8b2646bc1046386b9028e3ca2245ffed81efd392
Alina Molot <Alina_Molot@epam.com>
2018-03-15 19:04:23 +0200

    Another try

--- src/main/html/ApplicationForm.js
+++ src/main/html/ApplicationForm.js
@@ -1,28 +1,53 @@
-function testLogin (login) {
-beforePattern = /\b[\wа-я]{1,3}@[\w.а-я]+\.[\wа-я]+\b/;
-beforePattern2 = /\b[\wа-я]{20,}@[\w.а-я]+\.[\wа-я]+\b/;
-digitalPattern= /\b\d+@\d+\.\d+\b/;
-afterPattern= /\b[\wа-я]+@[\w.а-я]{1}\.[\wа-я]+\b/;
-afterPattern2= /\b[\wа-я]{4,20}@[\w.а-я]{6,}\.[\wа-я]+\b/;
+    function testLogin() {
+        var login = document.getElementById('login');
+        pattern = /([\w-\.а-яА-я]{4,20}@([\wа-яА-Я]{2,5}.){1,3})/
+        
+        if (!login.value.match(pattern)) {
+            success = false;
+            alert ('Логин неверный');    
+        }
+    }
+    function testPass() {
+        var password = document.getElementById('password');
+        allDigitPattern = /[\d]{8,20}/;
+        charPattern = /[a-z]{8,20}/;
+        noSymbPattern = /\b[\w\d]{8,20}/;
+        forbiddenSymbPattern = /[^~":;<>]{3,5}/;
+        shortPattern = /\b[\w\d!%&@*]{,8}/;
+        longPattern = /\b[\w\d!%&@*]{20,}/;
 
-if (digitalPattern.test(login)===true) {
-alert ('Логин не может состоять только из цифр'); return false;
-return true;
-}
-if (beforePattern.test(login)===true) {
-alert ('Имя пользователя не может быть меньше 4 символов'); return false;
-return true;
-}
-if (beforePattern2.test(login)===true) {
-alert ('Имя пользователя не может быть больше 20 символов'); return false;
-return true;
-}
-if (afterPattern.test(login)===true) {
-alert ('Имя домена должно быть больше 2 символов (до 5)'); return false;
-return true;
-}
-if (afterPattern2.test(login)===true) {
-alert ('Имя домена должно быть до 5 символов (больше 2)'); return false;
-return true;
-}
-}
+
+        if (password.value.match(allDigitPattern)) {
+            success = false;
+            alert ('Пароль не может состоять только из цифр.Включите буквы и от 3 до 5 символов !%&@*');
+        }
+        if (password.value.match(charPattern)){
+            success = false;
+            alert('Пароль не может состоять только из букв.Включите цифры и от 3 до 5 символов !%&@*');  
+        }
+        if (password.value.match(noSymbPattern)) {
+            success = false;
+            alert('В пароле нет символов.Включите от 3 до 5 символов !%&@*');
+        }
+        if (password.value.match(forbiddenSymbPattern)) {
+            success = false;
+            alert('Введенные в пароле символы не допускаются.Включите от 3 до 5 символов !%&@*');s
+        }
+        if (password.value.match(shortPattern)) {
+            success = false;
+            alert('Введенный пароль слишком короткий. Допустимо от 9 до 20 символов'); 
+        }
+        if (password.value.match(longPattern)) {
+            success = false;
+            alert('Введенный пароль слишком длинный. Допустимо от 9 до 20 символов');
+        }
+    }
+    
+    function checkForm() {
+        success = true;
+        testLogin();
+        testPass();
+        if(success == true){
+            document.location.href='fileshareLogged.html';
+        }
+    }
